bc592790b1260c083e44214f29359392c6111898
kaguithiajohnson@gmail.com <kaguithiajohnson@gmail.com>
2022-11-23 19:32:27 +0300

    Enable payments by coupons

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -125,9 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
-MEDIA_url = '/media/'
+MEDIA_URL= '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
