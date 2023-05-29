eed7bba8d80e82fed30fcc22ae4a435f56c53d25
kaguithiajohnson@gmail.com <kaguithiajohnson@gmail.com>
2023-05-29 12:58:46 +0300

    Fix vercel deployment configurations.

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,4 @@
-# build_files.sh
-pip3 install -r requirements.txt
-
-# make migrations
-python3 manage.py migrate
-python3 manage.py collectstatic
+ echo "BUILD START"
+ python3.9 -m pip install -r requirements.txt
+ python3.9 manage.py collectstatic --noinput --clear
+ echo "BUILD END"
